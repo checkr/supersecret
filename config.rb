179d17f6b9b732628765b56a76a934667f8ebdd9
Rex Salisbury <rex.salisbury@gmail.com>
2017-02-15 13:59:05 -0800

    remove password and other secret stuff

---
+++ config.rb
@@ -0,0 +1,12 @@
+# _________ .__                   __            
+# \_   ___ \|  |__   ____   ____ |  | _________ 
+# /    \  \/|  |  \_/ __ \_/ ___\|  |/ /\_  __ \
+# \     \___|   Y  \  ___/\  \___|    <  |  | \/
+#  \______  /___|  /\___  >\___  >__|_ \ |__|   
+#         \/     \/     \/     \/     \/        
+
+# is hiring.... ;)
+# bit.ly/checkr-full-stack
+# bit.ly/checkr-backend-eng
+# bit.ly/checkr-ml-eng
+# all jobs: bit.ly/checkr-jobs
